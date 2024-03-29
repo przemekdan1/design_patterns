430596e4958e86e0d9f77c313305ec19744c6861
przemekdan1 <jange1337@interia.pl>
2024-03-29 13:33:23 +0100

    Update README.md

--- obj/Debug/net8.0/design_patterns.AssemblyInfo.cs
+++ obj/Debug/net8.0/design_patterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("design_patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e75146f228a2ac4592ba473214c2579da78511f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("design_patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("design_patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
